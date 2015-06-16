fcebb34e13aeab02cdf3343728d7880e659bef29
xujh <1611992451@qq.com>
2015-06-16 15:03:12 +0800

    我一开始练习

--- stepByStep/channel-synchronization.go
+++ stepByStep/channel-synchronization.go
@@ -4,5 +4,15 @@ import "fmt"
 import "time"
 
 func worker(done chan bool){
+	fmt.Print("working....")
+	time.Sleep(time.Second)
+	fmt.Println("done")
 
+	done<-true
+}
+
+func main(){
+	done := make(chan bool,1)
+	go worker(done)
+	<- done
 }
