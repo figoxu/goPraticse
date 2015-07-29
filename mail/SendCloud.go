c3ed33889d34965dbd0a33869519ddf330fc0c0f
xujh <xujianhui@mrocker.com>
2015-07-29 14:53:54 +0800

    format code

--- mail/SendCloud.go
+++ mail/SendCloud.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/quexer/utee"
 	"net/url"
-	"fmt"
 )
 
 func main() {
@@ -11,7 +11,7 @@ func main() {
 	apiUser := "xujianhui_test_4TyfVp"
 	apiKey := "3p03OLGHQ1W4cCnv"
 
-	q :=  url.Values{}
+	q := url.Values{}
 	q.Add("api_user", apiUser)
 	q.Add("api_key", apiKey)
 
@@ -22,11 +22,9 @@ func main() {
 	q.Add("html", "你太 棒了！你已成功的从SendCloud发送了一封测试邮件，接下来快登录前台去完善账户信息吧！")
 	q.Add("resp_email_id", "true")
 
-
-
-	b ,err := utee.HttpPost(address, q)
-	if err!=nil{
-		fmt.Print("err:",err)
+	b, err := utee.HttpPost(address, q)
+	if err != nil {
+		fmt.Print("err:", err)
 	}
-	fmt.Print("@rsp:"+string(b))
+	fmt.Print("@rsp:" + string(b))
 }
