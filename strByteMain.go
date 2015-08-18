3fbc21fcc7cc1c098967ba3caa24f93c874bb0de
xujh <xujianhui@mrocker.com>
2015-08-18 10:04:04 +0800

    str demo

---
+++ strByteMain.go
@@ -0,0 +1,29 @@
+package main
+import (
+	"fmt"
+	"strings"
+	"strconv"
+)
+
+
+
+func main(){
+	v := fmt.Sprint("hellohowareyou",",1");
+	d := []byte(v)
+
+
+	v2 := string(d)
+
+	fmt.Println("==================[1]===============")
+	fmt.Println(v)
+	fmt.Println("==================[2]===============")
+	fmt.Println(d)
+	fmt.Println("==================[3]===============")
+	fmt.Println(v2)
+
+	s := strings.Split("abc,abc", ",")
+	fmt.Println(s, len(s))
+
+	v_int,_ := strconv.Atoi("1024")
+	fmt.Println(v_int)
+}
