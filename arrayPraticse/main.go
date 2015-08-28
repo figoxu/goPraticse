98e41462941e983065d33c6c64557b1b13c9d5f7
xujh <xujianhui@mrocker.com>
2015-08-28 23:20:32 +0800

    array praticse

---
+++ arrayPraticse/main.go
@@ -0,0 +1,21 @@
+package main
+import "fmt"
+
+
+func main(){
+	fmt.Println("hello")
+
+
+
+
+	fvv := []string{}
+	fvv = append(fvv,"hello")
+	fvv = append(fvv,"how")
+	fvv = append(fvv,"are")
+	fvv= append(fvv,"you")
+	fvv = append(fvv,"i am")
+	fvv = append(fvv,"fine")
+	fvv = append(fvv,"thank")
+	fvv = append(fvv,"you")
+	fmt.Println(fvv)
+}
