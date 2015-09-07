58f687017bc5bd93a275407fc6c6a480781637a7
xujh <xujianhui@mrocker.com>
2015-09-07 17:39:43 +0800

    json 序列化 反序列化

---
+++ json/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Connector struct {
+	Id     string
+	Weight int
+}
+
+var (
+	m = map[string]*Connector{}
+)
+
+func main() {
+	append := func(id string, weight int) {
+		m[id] = &Connector{
+			Id:     id,
+			Weight: weight,
+		}
+	}
+	for i := 0; i < 100; i++ {
+		v := fmt.Sprint("test", i)
+		append(v, i)
+	}
+
+	b, err := json.Marshal(m)
+	if err != nil {
+		fmt.Println("@err:", err)
+	}
+	conf := string(b)
+
+	fmt.Println("@val:" + conf)
+
+
+
+
+	var result map[string]*Connector
+	json.Unmarshal([]byte(conf), &result)
+
+
+
+	fmt.Println("@result:",result)
+	fmt.Println(result["test98"].Weight)
+}
