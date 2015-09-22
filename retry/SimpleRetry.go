da9444c39e200fdcb5577acfd70f108bba7b37a5
xujh <xujianhui@mrocker.com>
2015-09-22 11:56:44 +0800

    retry with 3 times

---
+++ retry/SimpleRetry.go
@@ -0,0 +1,23 @@
+package main
+import (
+	"fmt"
+	"log"
+	"errors"
+)
+
+func main(){
+	fmt.Println("hello")
+
+
+	err := someBusiness()
+	retry :=0
+	for err != nil && retry<3 {
+		retry ++
+		log.Println("no dv stat @retry:",retry)
+	}
+
+}
+
+func someBusiness() error{
+	return  errors.New("some err")
+}
