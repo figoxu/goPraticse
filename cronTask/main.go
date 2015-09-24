e1ccf70e310b4af6bed209eb60fe0fd8beca7478
xujh <xujianhui@mrocker.com>
2015-09-24 11:40:03 +0800

    add cron sample

---
+++ cronTask/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/robfig/cron"
+	"log"
+)
+
+func main() {
+	i := 0
+	c := cron.New()
+	spec := "*/5 * * * * ?"
+	c.AddFunc(spec, func() {
+		i++
+		log.Println("cron running:", i)
+	})
+	c.Start()
+
+	select {}
+}
