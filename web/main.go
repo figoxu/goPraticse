1d922b381d4c5b01ad6bca80a2d3e368f8271808
xujh <xujianhui@mrocker.com>
2015-09-25 15:14:23 +0800

    web sample

---
+++ web/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/gzip"
+	"github.com/martini-contrib/render"
+	"github.com/quexer/utee"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("hello")
+	m := martini.Classic()
+	m.Handlers(gzip.All(), martini.Recovery())
+	Mount(m, 100)
+	log.Printf("start gateway on %s\n", 5050)
+
+	log.Fatal(http.ListenAndServe(":5050", nil))
+//	m.RunOnAddr(":5050")
+}
+
+func Mount(m *martini.ClassicMartini, concurrent int) {
+
+
+	fmt.Println("concurrent @v:", concurrent)
+	m.Use(utee.MidConcurrent(concurrent))
+	m.Use(render.Renderer())
+	m.Use(utee.MidTextDefault)
+	//	//map request web utilities
+	m.Use(func(w http.ResponseWriter, c martini.Context) {
+		web := &utee.Web{W: w}
+		c.Map(web)
+	})
+
+	m.Group("/hello", func(r martini.Router) {
+		r.Post("/world", test)
+		r.Get("/world", test)
+	})
+	http.Handle("/", m)
+//	http.HandleFunc("/test/a", testhandler)
+}
+
+func test(r *http.Request, web *utee.Web) (int, string) {
+	return web.Json(200, "hello world")
+}
+
+func testhandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("i am in")
+}
