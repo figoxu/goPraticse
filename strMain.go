7f6365f1c3ea6d76025d6706fb67468171d67380
xujh <xujianhui@mrocker.com>
2015-10-27 15:43:26 +0800

    string + 运算符

--- strMain.go
+++ strMain.go
@@ -2,10 +2,14 @@ package main
 import (
 	"log"
 	"bytes"
+	"github.com/quexer/utee"
+	"strconv"
 )
 
 func main(){
+	t:=utee.Tick()
+	log.Println("@t:",t)
 	buf := bytes.NewBufferString("hello")
-	buf.WriteString(" world")
+	buf.WriteString(" world @time:"+strconv.FormatInt(t,10))
 	log.Println(buf.String())
 }
