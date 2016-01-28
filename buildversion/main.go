14cb94ef8d7d6bebeb85b38c14198d25031cb088
xujh <xujianhui@mrocker.com>
2016-01-28 15:56:51 +0800

    pack version info

---
+++ buildversion/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var Version = "No Version Provided"
+
+func main() {
+	fmt.Printf("App Version: %s\n", Version)
+}
---
+++ buildversion/versionBuildAndRun.sh
@@ -0,0 +1,5 @@
+hcode=`git rev-parse HEAD`
+u=`whoami`
+bd=`date "+%Y_%m_%d_%H:%M:%S"`
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-X main.Version $u$bd$hcode" &&  ./buildversion
