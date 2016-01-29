ea234a7690334475ee45104a2481ee6f42a72ce1
xujh <xujianhui@mrocker.com>
2016-01-29 09:54:21 +0800

    huffman coding

---
+++ algorithm/huffman/main.go
@@ -0,0 +1,11 @@
+package main
+import (
+	"github.com/Focinfi/ghuffmancoding"
+	"log"
+)
+//http://www.cnblogs.com/huangxincheng/archive/2012/12/09/2809993.html
+func main(){
+
+	result := ghuffmancoding.Encode("111223")
+	log.Println(result)
+}
