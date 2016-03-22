b1b5c6f824a02c435f042318248906bb4711537e
xujh <xujianhui@mrocker.com>
2016-03-22 14:41:08 +0800

    generate excel file type "xls"

---
+++ 3rd/excel/writeExcel/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/csv"
+	"os"
+)
+
+func main() {
+	f, err := os.Create("./test.xls")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	f.WriteString("\xEF\xBB\xBF") // 写入UTF-8 BOM
+	w := csv.NewWriter(f)
+	w.Write([]string{"编号", "姓名", "年龄"})
+	w.Write([]string{"1", "张三", "23"})
+	w.Write([]string{"2", "李四", "24"})
+	w.Write([]string{"3", "王五", "25"})
+	w.Write([]string{"4", "赵六", "26"})
+	w.Flush()
+}
