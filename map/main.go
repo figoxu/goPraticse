829f0065f277ba26bf39c4a78d198eef67882000
xujh <xujianhui@mrocker.com>
2016-03-28 16:55:12 +0800

    json map interface

--- map/main.go
+++ map/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "log"
+import (
+	"log"
+	"encoding/json"
+	"github.com/figoxu/utee"
+)
 
-func main(){
+var bm map[string]interface{} //key : made
+
+type Info struct {
+	Made     string `json:"made"`					//首字母需要大写
+	Daily    int    `json:"daily"`
+	Monthly  int    `json:"monthly"`
+	Total    int    `json:"total"`
+	UnqTotal int    `json:"unqTotal"`
+}
+
+func main() {
 	m := make(map[string]string)
 	m["hello"] = "echo hello"
 	m["world"] = "echo world"
@@ -17,4 +31,13 @@ func main(){
 	for k := range m {
 		log.Println(k)
 	}
+
+	bm = make(map[string]interface{})
+	bm["a"] = Info{
+		Made:"test",
+		Daily:100,
+	}
+	b,e:=json.Marshal(bm)
+	utee.Chk(e)
+	log.Println(string(b))
 }
