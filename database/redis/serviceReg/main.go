6e5593b3737d246e0ad596b2439fc93a42cc7d77
jianhui xu <jianhui.xu@tendcloud.com>
2016-07-07 17:33:38 +0800

    refactor: let health method belong to a struct

--- database/redis/serviceReg/main.go
+++ database/redis/serviceReg/main.go
@@ -9,13 +9,12 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
 var (
-	rp        *redis.Pool
-	keyMe     string
-	healthMap = make(map[string]*HealthApi)
+	rp *redis.Pool
 )
 
 const (
@@ -27,22 +26,67 @@ func main() {
 	rp = utee.CreateRedisPool(30, "106.75.27.144:6379", "baulk3?speed")
 	addr := flag.String("p", ":6666", "address where the server listen on")
 	flag.Parse()
-	keyMe = healthKey(*addr)
-	log.Println("@key:", keyMe)
-
-	c := cron.New()
-	c.AddFunc(cron_healthReport_task, func() {
-		healthReport(keyMe)
-	})
-	c.AddFunc(cron_healthChk_task, healthCheck)
-	c.Start()
 	log.Printf("start server http 1 on %s \n", *addr)
-	http.HandleFunc("/api/health", healthCheckHandler)
+	guards := NewHealthGuards(*addr)
+	for _, v := range guards.m {
+		healthApi := v.(*HealthApi)
+		if v.key == guards.keyMe {
+			continue
+		}
+		uri := fmt.Sprint(healthApi.api, "/cache/clear/:id")
+		utee.HttpPost(uri, &url.Values{})
+	}
+	http.HandleFunc("/api/health", guards.healthCheckHandler)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 
 }
 
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+func NewHealthGuards(port string) *HealthGuards {
+	keyMe := ""
+	ifaces, err := net.Interfaces()
+	utee.Chk(err)
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		utee.Chk(err)
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			ipStr := ip.String()
+			if strings.Index(ipStr, "::") != -1 {
+				continue
+			}
+			if ipStr == "127.0.0.1" {
+				continue
+			}
+			if keyMe != "" {
+				keyMe = fmt.Sprint(keyMe, ",")
+			}
+			keyMe = fmt.Sprint(keyMe, "http://", ipStr, port)
+		}
+	}
+	healthGuard := &HealthGuards{
+		m:     make(map[string]*HealthApi),
+		keyMe: keyMe,
+	}
+
+	c := cron.New()
+	c.AddFunc(cron_healthReport_task, healthGuard.healthReport)
+	c.AddFunc(cron_healthChk_task, healthGuard.healthCheck)
+	c.Start()
+	return healthGuard
+}
+
+type HealthGuards struct {
+	m     map[string]*HealthApi
+	keyMe string
+}
+
+func (p *HealthGuards) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 	fmt.Fprint(w, "ok")
 	return
@@ -55,7 +99,7 @@ type HealthApi struct {
 	illCount int
 }
 
-func healthCheck() {
+func (p *HealthGuards) healthCheck() {
 	c := rp.Get()
 	defer c.Close()
 	keys, err := redis.Strings(c.Do("HKEYS", "service_gateway"))
@@ -67,84 +111,49 @@ func healthCheck() {
 		return false
 	}
 	for _, key := range keys {
-		if key == keyMe {
-			log.Println("is not need to check myself")
+		if key == p.keyMe {
 			continue
 		}
-		if healthMap[key] == nil {
-			healthMap[key] = &HealthApi{
+		if p.m[key] == nil {
+			p.m[key] = &HealthApi{
 				key:  key,
 				apis: strings.Split(key, ","),
 				api:  "",
 			}
 		}
-		if check(healthMap[key].api) {
+		if check(p.m[key].api) {
 			continue
 		}
 		healthFlag := false
-		for _, api := range healthMap[key].apis {
+		for _, api := range p.m[key].apis {
 			log.Println("@api:", api)
 			if b, err := utee.HttpGet(fmt.Sprint(api, "/api/health")); err == nil && string(b) == "ok" {
 				healthFlag = true
-				healthMap[key].api = api
-				log.Println("okay  @api:", api, "  ==[skip other api]==")
+				p.m[key].api = api
 				break
 			}
 		}
 		if !healthFlag {
 			log.Println("[warn] @host:", key, " is not health . ")
-			healthMap[key].illCount++
+			p.m[key].illCount++
 		}
-		if healthMap[key].illCount > 3 {
-			ill2die(key)
+		if p.m[key].illCount > 3 {
+			p.ill2die(key)
 		}
 	}
 
 }
 
-func ill2die(key string) {
+func (p *HealthGuards) ill2die(key string) {
 	log.Println("[warn] kick not health @host:", key, " from conf server")
-	delete(healthMap, key)
+	delete(p.m, key)
 	c := rp.Get()
 	defer c.Close()
 	c.Do("HDEL", "service_gateway", key)
 }
 
-func healthReport(key string) {
+func (p *HealthGuards) healthReport() {
 	c := rp.Get()
 	defer c.Close()
-	c.Do("HSETNX", "service_gateway", key, 1)
-}
-
-func healthKey(port string) string {
-	hk := ""
-	ifaces, err := net.Interfaces()
-	utee.Chk(err)
-	for _, i := range ifaces {
-		addrs, err := i.Addrs()
-		utee.Chk(err)
-		// handle err
-		for _, addr := range addrs {
-			var ip net.IP
-			switch v := addr.(type) {
-			case *net.IPNet:
-				ip = v.IP
-			case *net.IPAddr:
-				ip = v.IP
-			}
-			// process IP address
-			ipStr := ip.String()
-			if strings.Index(ipStr, "::") != -1 {
-				continue
-			}
-			if ipStr == "127.0.0.1" {
-				continue
-			}
-			if hk != "" {
-				hk = fmt.Sprint(hk, ",")
-			}
-			hk = fmt.Sprint(hk, "http://", ipStr, port)
-		}
-	}
-	return hk
+	c.Do("HSETNX", "service_gateway", p.keyMe, 1)
 }
