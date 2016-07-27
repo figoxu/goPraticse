97b8a4ce84005a435e4965fae017b1794e18815e
jianhui xu <jianhui.xu@tendcloud.com>
2016-07-27 15:16:20 +0800

    布隆过滤器内存消耗测试

--- algorithm/bloomFilter/willfBloom/main.go
+++ algorithm/bloomFilter/willfBloom/main.go
@@ -5,22 +5,38 @@ import (
 	"github.com/zhenjl/cityhash"
 	"log"
 	"fmt"
+	_ "expvar"
+	"net/http"
+	_ "net/http/pprof"
+	"runtime/debug"
 )
 
 func main() {
+	go generateGarbage();
+	log.Fatal(http.ListenAndServe(":6666", nil))
+}
+//10亿设备量的过滤器，占用1.8G内存
+func generateGarbage(){
 	var capSize uint = 1000000000
 	filter := standard.New(capSize)
 	filter.SetHasher(cityhash.New64())
 	v := []byte("Love")
 	b := filter.Add(v).Check(v)
 	log.Println("check @v:", b)
-	for i:=0;i<100000000;i++ {
+	bad := 0
+	for i:=0;i<1000000000;i++ {
+		if i%1000000 == 0 {
+			log.Println(i)
+			debug.FreeOSMemory()
+		}
 		d := []byte( fmt.Sprint("data",i) )
 		if filter.Check(d) ==true{
-			panic(fmt.Sprint("should not exist @d:",string(d)))
+			bad++
+			//			panic(fmt.Sprint("should not exist @d:",string(d)))
 		}
 		if flag := filter.Add(d).Check(d) ;flag==false {
 			panic(d)
 		}
 	}
+	log.Println("====>>That is all @bad:",bad)
 }
