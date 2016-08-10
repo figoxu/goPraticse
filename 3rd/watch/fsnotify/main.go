01e329db4a5e195589e92b109354815af8c26f41
jianhui xu <jianhui.xu@tendcloud.com>
2016-08-10 14:45:56 +0800

    file watch notify

---
+++ 3rd/watch/fsnotify/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"github.com/howeyc/fsnotify"
+)
+
+func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	done := make(chan bool)
+
+	// Process events
+	go func() {
+		for {
+			select {
+			case ev := <-watcher.Event:
+				log.Println("event:", ev)
+			case err := <-watcher.Error:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	dir4watch := "./"
+	err = watcher.Watch(dir4watch)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Hang so program doesn't exit
+	<-done
+
+	/* ... do stuff ... */
+	watcher.Close()
+}
