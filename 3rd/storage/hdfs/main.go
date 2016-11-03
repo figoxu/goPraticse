f8cbc1935008c7351dd21776cf1a5cff7949b935
Figo.Xu <xujh945@qq.com>
2016-11-03 15:29:54 +0800

    All HDFS With Figo's Lib

--- 3rd/storage/hdfs/main.go
+++ 3rd/storage/hdfs/main.go
@@ -1,29 +1,17 @@
 package main
 
 import (
-	"github.com/colinmarc/hdfs"
 	"github.com/figoxu/Figo"
 	"github.com/quexer/utee"
 	"log"
-	"os"
 )
 
 func main() {
 	defer Figo.Catch()
-	os.Setenv("HADOOP_USER_NAME", "root")
-	client, _ := hdfs.New("192.168.108.131:9000")
-	fname := "/figo/tmp/how/are/you/figoTmp2016.txt"
-	client.MkdirAll("/figo/tmp/how/are/you/", 0644)
-	w, err := client.Create(fname)
-	utee.Chk(err)
-	v, err := w.Write([]byte("Hello Figo Over There"))
-	log.Println("@v:", v)
-	utee.Chk(err)
-	log.Println("@v:", v)
-	w.Close()
-	b, err := client.ReadFile(fname)
-	utee.Chk(err)
-	log.Println("===[Log I Read Is]===")
-	log.Println(string(b))
-	log.Println("===[Over]===")
+	hdfsClient := Figo.NewHDFSClient("192.168.108.131:9000", "root")
+	fullPath := "/figo/github/foo.txt"
+	hdfsClient.Write(fullPath, []byte("Here We Go,Frightting To Eat"))
+	v, e := hdfsClient.Read(fullPath)
+	utee.Chk(e)
+	log.Println("@value I Read is :", string(v))
 }
