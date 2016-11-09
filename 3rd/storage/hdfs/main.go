bbc6128542320b7949819669fe44d62f552837c3
Figo.Xu <xujh945@qq.com>
2016-11-09 10:22:29 +0800

    Change Ip And Directory For Hadoop

--- 3rd/storage/hdfs/main.go
+++ 3rd/storage/hdfs/main.go
@@ -8,9 +8,10 @@ import (
 
 func main() {
 	defer Figo.Catch()
-	hdfsClient := Figo.NewHDFSClient("192.168.108.131:9000", "root")
-	fullPath := "/figo/github/foo.txt"
-	hdfsClient.Write(fullPath, []byte("Here We Go,Frightting To Eat"))
+	hdfsClient := Figo.NewHDFSClient("172.17.0.3:9000", "root")
+	fullPath := "/user/root/foo.txt"
+	err:=hdfsClient.Write(fullPath, []byte("Here We Go,Frightting To Eat"))
+	utee.Chk(err)
 	v, e := hdfsClient.Read(fullPath)
 	utee.Chk(e)
 	log.Println("@value I Read is :", string(v))
