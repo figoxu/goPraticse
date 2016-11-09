0021a95eaae920c3ee93a3c999db14d3178f4e26
Figo.Xu <xujh945@qq.com>
2016-11-09 10:31:14 +0800

    Defer Execute Order : Upgrade

--- languageGarama/defer/main.go
+++ languageGarama/defer/main.go
@@ -9,14 +9,14 @@ import (
 func main(){
 	defer log.Println("defer 001")
 	defer log.Println("defer 002")
-	defer log.Println("defer 003")
 	defer Figo.Catch()
+	defer log.Println("defer 003")
 	log.Println("execute 001")
 	utee.Chk(errors.New("Error"))
 	log.Println("execute 002")
 }
 //2016/11/09 10:28:58 execute 001
-//2016/11/09 10:28:58 Error  (recover)
 //2016/11/09 10:28:58 defer 003
+//2016/11/09 10:28:58 Error  (recover)
 //2016/11/09 10:28:58 defer 002
 //2016/11/09 10:28:58 defer 001
