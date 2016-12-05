558dbac8fbe4e75eb3e55a25d7ee963ad7e4a096
Figo.Xu <xujh945@qq.com>
2016-12-05 17:49:48 +0800

    Add CustomType Decoder For Form Parse

--- 3rd/httpForm/formam/main.go
+++ 3rd/httpForm/formam/main.go
@@ -47,8 +47,11 @@ func Handler(w http.ResponseWriter, r *http.Request) error {
 		Interface: &InterfaceStruct{}, // its is possible to access to the fields although it's an interface field!
 	}
 	r.ParseForm()
-	dec := formam.NewDecoder(&formam.DecoderOptions{TagName: "formam"})
-	if err := dec.Decode(r.Form, &m); err != nil {
+	decoder := formam.NewDecoder(&formam.DecoderOptions{TagName: "formam"})
+	decoder.RegisterCustomType(func(vals []string) (interface{}, error) {
+		return time.Parse("2006-01-02", vals[0])
+	}, []interface{}{time.Time{}}, nil)
+	if err := decoder.Decode(r.Form, &m); err != nil {
 		return err
 	}
 	log.Println("@company:", m)
