1c383a27a767b24345942dae0029ad1f8cc5391f
Figo.Xu <xujh945@qq.com>
2017-02-08 17:51:57 +0800

    phantomjs sample

---
+++ 3rd/library/phantomjs/main.go
@@ -0,0 +1,24 @@
+package main
+import (
+	"github.com/urturn/go-phantomjs"
+	"fmt"
+)
+
+func main() {
+	p, err := phantomjs.Start()
+	if err != nil {
+		panic(err)
+	}
+	defer p.Exit() // Don't forget to kill phantomjs at some point.
+	var result interface{}
+	err = p.Run("function() { return 2 + 2 }", &result)
+	if err != nil {
+		panic(err)
+	}
+	number, ok := result.(float64)
+	if !ok {
+		panic("Cannot convert result to float64")
+	}
+	fmt.Println(number)
+	// Output: 4
+}
---
+++ 3rd/library/phantomjs/readme.md
@@ -0,0 +1,1 @@
+http://phantomjs.org/
