b79a752bfa06392e41deed84034063588203b6dc
Figo.Xu <xujh945@qq.com>
2017-03-30 11:22:25 +0800

    log anaylist

--- tools/log_analyst/main.go
+++ tools/log_analyst/main.go
@@ -5,24 +5,47 @@ import (
 	"github.com/figoxu/Figo"
 	"io/ioutil"
 	"github.com/quexer/utee"
+	"fmt"
 )
 
 func main(){
-
 	logPath := "/home/figo/delete_it/log.20170330.err"
+	packageStr := "talkingdata"
+	logAnalysist(logPath,packageStr)
+}
+
+
+func logAnalysist(logPath,packageStr string){
+
+	type Item struct {
+		key string
+		val int32
+	}
+
+	sortPrint:=func (items []Item){
+		for i := 0; i < len(items); i++ {
+			for j := 0; j < len(items)-i-1; j++ {
+				if items[j].val < items[j+1].val {
+					items[j], items[j+1] = items[j+1], items[j]
+				}
+			}
+		}
+		for _,item := range items {
+			log.Println("代码:",item.key,"  出错次数:",item.val)
+		}
+	}
+
 	b,err:=ioutil.ReadFile(logPath)
 	utee.Chk(err)
 	parser := Figo.Parser{
-		PrepareReg:[]string{"talkingdata.+","\\(.+java.+[0-9]+\\)"},
+		PrepareReg:[]string{fmt.Sprint(packageStr,".+"),"\\(.+java.+[0-9]+\\)"},
 		ProcessReg:[]string{},
 	}
-	log.Println("hello")
 	contents := parser.Exe(string(b))
-
+	m := map[string]int32{}
 	for _,content:= range contents {
-		inc(content)
+		m[content]=m[content]+1
 	}
-
 	items := []Item{}
 	for k,v := range m {
 		items = append(items,Item{
@@ -31,30 +54,4 @@ func main(){
 		})
 	}
 	sortPrint(items)
-}
-
-var (
-	m = map[string]int32{}
-)
-
-func inc(key string){
-	m[key]=m[key]+1
-}
-
-type Item struct {
-	key string
-	val int32
-}
-
-func sortPrint(items []Item){
-	for i := 0; i < len(items); i++ {
-		for j := 0; j < len(items)-i-1; j++ {
-			if items[j].val < items[j+1].val {
-				items[j], items[j+1] = items[j+1], items[j]
-			}
-		}
-	}
-	for _,item := range items {
-		log.Println("代码:",item.key,"  出错次数:",item.val)
-	}
 }
