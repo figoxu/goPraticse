0963eef59518d9ec64240baf86f5f8eb4c616bf8
Figo.Xu <xujh945@qq.com>
2017-07-30 20:59:35 +0800

    Ping 日志分析，增加容错

--- regexpSample/logRegXp/main.go
+++ regexpSample/logRegXp/main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 	"github.com/figoxu/Figo"
 	"fmt"
+	"log"
 )
 
 var local *time.Location
@@ -16,7 +17,7 @@ func init(){
 }
 
 func main(){
-	b,err:=ioutil.ReadFile("/Users/xujianhui/Documents/err02.log")
+	b,err:=ioutil.ReadFile("/Users/xujianhui/Documents/err.log")
 	utee.Chk(err)
 	contents:=strings.Split(string(b),"--")
 	for _,content:=range contents {
@@ -28,11 +29,21 @@ func main(){
 }
 
 func minTime(feature,content string) time.Time{
+	defer Figo.Catch()
 	parser:=Figo.Parser{
 		PrepareReg:[]string{fmt.Sprint(".+",feature),"\\d+/\\d+/\\d+ \\d+:\\d+:\\d+"},
 		ProcessReg: []string{},
 	}
-	t,err:= time.ParseInLocation("2006/01/02 15:04:05",parser.Exe(content)[0],local)
-	utee.Chk(err)
+	contents := parser.Exe(content)
+	parseT := func (str string) (time.Time, error){
+		defer Figo.Catch()
+		return time.ParseInLocation("2006/01/02 15:04:05", str,local)
+	}
+	t,err:= parseT(contents[0])
+	if err!=nil {
+		defer Figo.Catch()
+		log.Println("@content[0]",contents[0]," @content[1]:",contents[1])
+		t,err= parseT(contents[1])
+	}
 	return t
 }
