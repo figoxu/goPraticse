b2ecd8aea56cb0576e452118828d54b9fc0af789
figoxu <xujh945@qq.com>
2017-11-14 20:44:48 +0800

    增加byte数组变成Ip和端口的字符串

--- str/ip2byte/main.go
+++ str/ip2byte/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/figoxu/Figo"
 	"log"
 	"strings"
@@ -9,10 +10,15 @@ import (
 func main() {
 	log.Println(parseIp("59.110.166.246:5052"))
 	log.Println(parseIp("8.8.8.8:65535"))
-	log.Println(parseIp("255.255.255.0:65535"))
-	log.Println(parseIp2bytes("59.110.166.246:5052"))
-	log.Println(parseIp2bytes("8.8.8.8:65535"))
-	log.Println(parseIp2bytes("255.255.255.0:65535"))
+	log.Println(parseIp("255.255.255.0:255"))
+	bs := parseIp2bytes("59.110.166.246:5052")
+	log.Println(bs, "   ", parsebytes2IpAddr(bs))
+	bs = parseIp2bytes("8.8.8.8:65535")
+	log.Println(bs, "   ", parsebytes2IpAddr(bs))
+	bs = parseIp2bytes("255.255.255.0:255")
+	log.Println(bs, "   ", parsebytes2IpAddr(bs))
+	bs = parseIp2bytes("127.0.0.1:256")
+	log.Println(bs, "   ", parsebytes2IpAddr(bs))
 }
 
 func parseIp(ipAddr string) (ips []byte, port uint16) {
@@ -39,4 +45,10 @@ func parseIp2bytes(ipAddr string) (bs []byte) {
 	bs = append(bs, ips...)
 	bs = append(bs, h, l)
 	return bs
+}
+
+func parsebytes2IpAddr(bs []byte) (addr string) {
+	addr = fmt.Sprint(bs[0], ".", bs[1], ".", bs[2], ".", bs[3])
+	addr = fmt.Sprint(addr, ":", int(bs[4])*256+int(bs[5]))
+	return addr
 }
