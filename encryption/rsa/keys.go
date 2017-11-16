ee6b7d317df11c973f060d52e28d2ec66d054ddf
figoxu <xujh945@qq.com>
2017-11-16 10:05:24 +0800

    把key抽取到keys.go的文件里

--- encryption/rsa/keys.go
+++ encryption/rsa/keys.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"encoding/base64"
-	"encoding/pem"
-	"errors"
-	"fmt"
-	"github.com/figoxu/Figo"
-	"log"
-)
 
 // 可通过openssl产生
 //openssl genrsa -out rsa_private_key.pem 1024
@@ -42,46 +31,3 @@ o5+qr5al5KibeJq3xyI/626IBsDMFX5o3WOoXceTF7+lgi6r+OuokqFJgpeh7YAN
 XQ8Y8mn8ucw+Ly+LbQIDAQAB
 -----END PUBLIC KEY-----
 `)
-
-// 加密
-func RsaEncrypt(origData []byte) ([]byte, error) {
-	//解密pem格式的公钥
-	block, _ := pem.Decode(publicKey)
-	if block == nil {
-		return nil, errors.New("public key error")
-	}
-	// 解析公钥
-	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
-	if err != nil {
-		return nil, err
-	}
-	// 类型断言
-	pub := pubInterface.(*rsa.PublicKey)
-	//加密
-	return rsa.EncryptPKCS1v15(rand.Reader, pub, origData)
-}
-
-// 解密
-func RsaDecrypt(ciphertext []byte) ([]byte, error) {
-	//解密
-	block, _ := pem.Decode(privateKey)
-	if block == nil {
-		return nil, errors.New("private key error!")
-	}
-	//解析PKCS1格式的私钥
-	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
-	if err != nil {
-		return nil, err
-	}
-	// 解密
-	return rsa.DecryptPKCS1v15(rand.Reader, priv, ciphertext)
-}
-
-func main() {
-	data, _ := RsaEncrypt([]byte("test dataΩ......"))
-	fmt.Println(base64.StdEncoding.EncodeToString(data))
-
-	log.Println(Figo.Bh.BStr(data))
-	origData, _ := RsaDecrypt(data)
-	fmt.Println(string(origData))
-}
---
+++ encryption/rsa/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"github.com/figoxu/Figo"
+	"log"
+)
+
+// 加密
+func RsaEncrypt(origData []byte) ([]byte, error) {
+	//解密pem格式的公钥
+	block, _ := pem.Decode(publicKey)
+	if block == nil {
+		return nil, errors.New("public key error")
+	}
+	// 解析公钥
+	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	// 类型断言
+	pub := pubInterface.(*rsa.PublicKey)
+	//加密
+	return rsa.EncryptPKCS1v15(rand.Reader, pub, origData)
+}
+
+// 解密
+func RsaDecrypt(ciphertext []byte) ([]byte, error) {
+	//解密
+	block, _ := pem.Decode(privateKey)
+	if block == nil {
+		return nil, errors.New("private key error!")
+	}
+	//解析PKCS1格式的私钥
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	// 解密
+	return rsa.DecryptPKCS1v15(rand.Reader, priv, ciphertext)
+}
+
+func main() {
+	data, _ := RsaEncrypt([]byte("test dataΩ......"))
+	fmt.Println(base64.StdEncoding.EncodeToString(data))
+
+	log.Println(Figo.Bh.BStr(data))
+	origData, _ := RsaDecrypt(data)
+	fmt.Println(string(origData))
+}
