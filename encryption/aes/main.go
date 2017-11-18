dd36c19b307f36ccb73540b76dfb3be508013699
figoxu <xujh945@qq.com>
2017-11-18 20:50:02 +0800

    新增AES加密范例

--- encryption/aes/main.go
+++ encryption/aes/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/hex"
 	"github.com/figoxu/Figo"
 	"github.com/quexer/utee"
 	"log"
@@ -19,6 +21,23 @@ func main() {
 	bs, err = aesHelp.Decrypt(bs)
 	utee.Chk(err)
 	log.Println("解密后")
+	log.Println(Figo.Bh.BStr(bs))
+	log.Println(string(bs))
+
+	key, err := hex.DecodeString("535b02b78c856fe97c1d05fd9c177742")
+	utee.Chk(err)
+	iv, err := hex.DecodeString("c6997d77fdbff4dcb6bc3afc559bd6b5")
+	aesHelp = Figo.NewAesHelp(key, iv...)
+	content1, err := hex.DecodeString("2061F59B269FCA6535886706FCB82CF25FCC891F4AC116D7A4B33110E14F0DC857D2C3D44554B0AFBAA84C034F3AC7FD731B7B301AA1727F53CD76688DE9A1C99424081A3D5CADD5471871EC4E6284215AFDEF4A66548A44C226D9ED196CF029")
+	utee.Chk(err)
+	bs, err = aesHelp.Decrypt(content1)
+	utee.Chk(err)
+	log.Println(Figo.Bh.BStr(bs))
+	log.Println(string(bs))
+
+	content2, err := base64.StdEncoding.DecodeString(`IGH1myafymU1iGcG/Lgs8l/MiR9KwRbXpLMxEOFPDchX0sPURVSwr7qoTANPOsf9cxt7MBqhcn9TzXZojemhyZQkCBo9XK3VRxhx7E5ihCFa/e9KZlSKRMIm2e0ZbPAp`)
+	utee.Chk(err)
+	bs, err = aesHelp.Decrypt(content2)
 	log.Println(Figo.Bh.BStr(bs))
 	log.Println(string(bs))
 }
