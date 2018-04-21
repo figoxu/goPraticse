e66b60cb9b9edc143185645b1c9c47405d4c2df1
figoxu <xujh945@qq.com>
2018-04-21 15:42:35 +0800

    Gin:解析URL里的参数

--- gin_sample/first/main.go
+++ gin_sample/first/main.go
@@ -8,13 +8,19 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/hello/world/json", h_helloWorldJson)
+	r.GET("/hello/name/:name", h_helloByName)
 	r.Run(":8080")
 }
 
 func h_helloWorldJson(c *gin.Context) {
 	c.SecureJSON(http.StatusOK, map[string]string{
-		"foo":"bar",
-		"hello":"world",
-		"figo":"xu",
+		"foo":   "bar",
+		"hello": "world",
+		"figo":  "xu",
 	})
+}
+
+func h_helloByName(c *gin.Context) {
+	name := c.Param("name")
+	c.String(http.StatusOK, "Hello %s", name)
 }
