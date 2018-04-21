b82e8fd0c7cb45c88ac59ef5109a31139fa5c17c
figoxu <xujh945@qq.com>
2018-04-21 15:46:40 +0800

    Gin:Redirect Sample

--- gin_sample/first/main.go
+++ gin_sample/first/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"fmt"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/hello/world/json", h_helloWorldJson)
 	r.GET("/hello/name/:name", h_helloByName)
+	r.GET("/hello/redirect/:url", h_helloRedirect)
 	r.Run(":8080")
 }
 
@@ -23,4 +25,10 @@ func h_helloWorldJson(c *gin.Context) {
 func h_helloByName(c *gin.Context) {
 	name := c.Param("name")
 	c.String(http.StatusOK, "Hello %s", name)
+}
+
+// http://localhost:8080/hello/redirect/www.baidu.com
+func h_helloRedirect(c *gin.Context){
+	url:=c.Param("url")
+	c.Redirect(http.StatusMovedPermanently,fmt.Sprintf("http://%s",url))
 }
