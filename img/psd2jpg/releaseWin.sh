280be5b9cea1b32ea5ca152e8e1fcefda884a02d
figoxu <xujh945@qq.com>
2018-11-27 06:53:39 +0800

    psd2jpg:尝试增加图标

--- img/psd2jpg/releaseWin.sh
+++ img/psd2jpg/releaseWin.sh
@@ -1,1 +1,2 @@
-GOOS=windows GOARCH=amd64 go build
+rsrc -manifest main.exe.manifest -ico rc.ico -o psd2jpg.syso
+GOOS=windows GOARCH=amd64 go build -o psd2jpg.exe github.com/figoxu/goPraticse/img/psd2jpg
