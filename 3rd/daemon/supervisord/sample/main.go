071db1cb7e07f8d4b2bc832a814f17e6d5a9f47d
figoxu <xujh945@qq.com>
2018-12-11 11:06:44 +0800

    supervisor运行时，得到目录信息

--- 3rd/daemon/supervisord/sample/main.go
+++ 3rd/daemon/supervisord/sample/main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"log"
 	"time"
+	"path/filepath"
+	"os"
+	"strings"
 )
 
-func main(){
-	for  {
-		log.Println("Hello Figo.xu & Mr egg")
-		time.Sleep(time.Second*time.Duration(5))
+func main() {
+	for {
+		log.Println("Hello Figo.xu & Mr egg @Dir:", getCurrentDirectory(),"  @sample:",os.Args[0])
+		time.Sleep(time.Second * time.Duration(5))
 	}
 }
 
+func getCurrentDirectory() string {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.Replace(dir, "\\", "/", -1)
+}
