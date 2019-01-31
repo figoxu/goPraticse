9f5ddbcac558208a3bc2894efef1c8bf60568ccd
figoxu <xujh945@qq.com>
2019-01-31 09:41:24 +0800

    excel尝试多次Flush

--- 3rd/excel/writeExcel/main.go
+++ 3rd/excel/writeExcel/main.go
@@ -16,6 +16,7 @@ func main() {
 	w.Write([]string{"编号", "姓名", "年龄"})
 	w.Write([]string{"1", "张三", "23"})
 	w.Write([]string{"2", "李四", "24"})
+	w.Flush()
 	w.Write([]string{"3", "王五", "25"})
 	w.Write([]string{"4", "赵六", "26"})
 	w.Flush()
