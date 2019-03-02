cce5db7515552cd76a14a01e993c8d48b5e597ee
figoxu <xujh945@qq.com>
2019-03-02 10:22:50 +0800

    gorm : pluck

--- 3rd/gorm/ormsample/user_test.go
+++ 3rd/gorm/ormsample/user_test.go
@@ -26,3 +26,11 @@ func TestUserDao_InsertWithFriends(t *testing.T) {
 	env.db.Model(&user).Association("Friends").Append(&User{Name: "张三"}, &User{Name: "李四"})
 	fmt.Println("HELLO")
 }
+
+func TestUserDao_Ids(t *testing.T) {
+	ids := make([]int, 0)
+	env.db.Model(&User{}).Pluck("id", &ids)
+	for _, id := range ids {
+		fmt.Println(id)
+	}
+}
