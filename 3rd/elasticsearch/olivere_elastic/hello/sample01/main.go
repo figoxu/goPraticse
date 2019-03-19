190439d06e4a4b0465d1a180a600a837b444b44a
figoxu <xujh945@qq.com>
2019-03-19 10:52:28 +0800

    elastic.v5 的入门范例

--- 3rd/elasticsearch/olivere_elastic/hello/sample01/main.go
+++ 3rd/elasticsearch/olivere_elastic/hello/sample01/main.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"fmt"
 	"github.com/figoxu/Figo"
+	"github.com/icrowley/fake"
 	"github.com/quexer/utee"
+	"github.com/sirupsen/logrus"
 	"gopkg.in/olivere/elastic.v5"
-	"log"
+	"reflect"
 )
 
 var client *elastic.Client
@@ -27,13 +29,38 @@ type Employee struct {
 func init() {
 
 	ctx := context.Background()
-	client, err := elastic.NewClient(elastic.SetSniff(false), elastic.SetURL(host), elastic.SetBasicAuth(account, password))
+	c, err := elastic.NewClient(elastic.SetSniff(false), elastic.SetURL(host), elastic.SetBasicAuth(account, password))
 	utee.Chk(err)
-	result, _, err := client.Ping(host).Do(ctx)
+	result, _, err := c.Ping(host).Do(ctx)
 	utee.Chk(err)
 	fmt.Println(Figo.JsonString(result))
+	client = c
 }
 
 func main() {
-	log.Println("Hello World")
+	tryInsert()
+	trySearch()
+}
+
+func tryInsert() {
+	indexService := client.Index().Index("test").Type("employee").Id("1").BodyJson(&Employee{
+		FirstName: fake.FirstName(),
+		LastName:  fake.LastName(),
+		Age:       18,
+		About:     "Love",
+		Interests: []string{"BasketBall", "PingPong", "FootBall"},
+	})
+	rsp, err := indexService.Do(context.Background())
+	utee.Chk(err)
+	logrus.WithField("rsp", rsp).WithField("err", err).Println("tryInsert")
+}
+
+func trySearch() {
+	q := elastic.NewQueryStringQuery("football")
+	rsp, err := client.Search().Index("test").Type("employee").Query(q).Size(10).Do(context.Background())
+	utee.Chk(err)
+	logrus.WithField("rsp", rsp).WithField("err", err).Println("trySearch")
+	for _, item := range rsp.Each(reflect.TypeOf(Employee{})) {
+		logrus.Println(Figo.JsonString(item))
+	}
 }
