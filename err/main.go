92522a521efb7ce01d4aee53d2cbd0d52eb42f8e
figoxu <xujh945@qq.com>
2019-05-04 15:27:25 +0800

    增加Pkg error的练习

--- err/main.go
+++ err/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"errors"
 	"github.com/figoxu/goPraticse/err/a"
+	"github.com/jinzhu/gorm"
+	pkgerror "github.com/pkg/errors"
 	"log"
 )
 
@@ -11,5 +13,11 @@ func main() {
 	e := errors.New("test")
 	log.Println(e.Error())
 	log.Println(e.Error() == "test")
+	errNotFound := pkgerror.WithStack(gorm.ErrRecordNotFound)
+	if pkgerror.Cause(errNotFound) == gorm.ErrRecordNotFound {
+		log.Println("SAME")
+	} else {
+		log.Panic("DIFFERENT")
+	}
 	a.TMethod()
 }
