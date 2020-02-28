fe787e9b5868dbc140e3a6183612539721f2eced
figoxu <xujh945@qq.com>
2020-02-28 18:15:18 +0800

    fix a concurrency bug

--- base/limitChan.go
+++ base/limitChan.go
@@ -4,24 +4,39 @@ import (
 	"log"
 	"sync"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
+type DataItem struct {
+	V int
+}
+
 func main() {
+	var vs []*DataItem
+	for i := 0; i < 100; i++ {
+		vs = append(vs, &DataItem{V: i})
+	}
 
 	var limitChan = make(chan struct{}, 10)
 	var waitGroup sync.WaitGroup
-	for i := 0; i < 100; i++ {
+	for _, v := range vs {
 		limitChan <- struct{}{}
 		waitGroup.Add(1)
-		j := i
+		item := v // 多一行赋值，可以去除线程安全问题
 		go func() {
 			defer waitGroup.Done()
 			defer func() {
 				<-limitChan
 			}()
-			log.Println(j)
+			log.Println(item.V)
+			item.V = item.V + 100
 			time.Sleep(time.Second * time.Duration(2))
 		}()
 	}
 	waitGroup.Wait()
+	logrus.Println("-------")
+	for _, v := range vs {
+		log.Println(v.V)
+	}
 }
