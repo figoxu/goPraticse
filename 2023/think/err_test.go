ce11cf486a02985666e4d095d16316331f0de94b
Figo.Xu <xujh945@qq.com>
2023-01-20 08:51:45 +0800

    minor test

--- 2023/think/err_test.go
+++ 2023/think/err_test.go
@@ -4,24 +4,19 @@ import (
 	"errors"
 	"figoxu/think"
 	"fmt"
-	"testing"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-var ErrTest = errors.New("error1")
+var _ = Describe("error_is_as_of_official_lib_after_1.13", func() {
+	var ErrTest = errors.New("error1")
+	err1 := fmt.Errorf("return err %w", think.TestError{Value: "error1"})
+	err2 := fmt.Errorf("inner error is %w", ErrTest)
 
-func returnError1() error {
-	return fmt.Errorf("return err %w", think.TestError{Value: "error1"})
-}
-
-func returnError2() error {
-	return fmt.Errorf("inner error is %w", ErrTest)
-}
-
-func TestErrorCheck(t *testing.T) {
-	err1 := returnError1()
-	err2 := returnError2()
-	fmt.Println("Error.as", errors.As(err1, &think.TestError{}))
-	fmt.Println("Error.is", errors.Is(err1, ErrTest))
-	fmt.Println("Error.as", errors.As(err2, &think.TestError{}))
-	fmt.Println("Error.is", errors.Is(err2, ErrTest))
-}
+	It("InfAnyNilMustNil", func() {
+		Ω(errors.As(err1, &think.TestError{})).Should(BeTrue())
+		Ω(errors.Is(err1, ErrTest)).Should(BeFalse())
+		Ω(errors.As(err2, &think.TestError{})).Should(BeFalse())
+		Ω(errors.Is(err2, ErrTest)).Should(BeTrue())
+	})
+})
